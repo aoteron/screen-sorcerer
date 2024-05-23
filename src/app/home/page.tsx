12b48e3e588fa574046f623c9027932b15745c8f
aoteron <aoteronunez@gmail.com>
2024-05-23 14:15:49 +0200

    Ready to start
    Dependencies installed and basic architecture

---
+++ src/app/home/page.tsx
@@ -0,0 +1,19 @@
+import '../styles/globals.css'
+
+export default function HomePage() {
+  return (
+    <div>
+      {/* Aquí va el header */}
+
+      <main>
+        <section>
+          <h1>Welcome to Your Dashboard</h1>
+          <p>This is your personalized dashboard.</p>
+        </section>
+        {/* Aquí iría el contenido adicional de la página */}
+      </main>
+
+      {/* Aquí va el footer */}
+    </div>
+  )
+}
